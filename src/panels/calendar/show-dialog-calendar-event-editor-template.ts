fe9b390fc1f0ed0136738583a862418c1b317bb1
tindrajargen <tindra@jargenstedt.se>
2024-11-22 08:08:26 +0100

    one extra file

---
+++ src/panels/calendar/show-dialog-calendar-event-editor-template.ts
@@ -0,0 +1,28 @@
+import { fireEvent } from "../../common/dom/fire_event";
+import type { CalendarEventData, CalendarTemplateEvents, CalendarTemplateViewEventItem } from "../../data/calendar";
+
+export interface CalendarEventEditTemplateDialogParams {
+//   calendarId?: string;
+  selectedDate?: Date; // When provided is used as the pre-filled date for the event creation dialog
+  entry?: CalendarEventData;
+  canDelete?: boolean;
+  updated: (events: CalendarTemplateViewEventItem[]) => void;
+  day: string;
+}
+
+export const loadCalendarEventEditTemplateDialog = () => {
+  console.log("Loading dialog-calendar-template-create...");
+  return import("./dialog-calendar-template-create");
+};
+
+
+export const showCalendarEventEditTemplateDialog = (
+  element: HTMLElement,
+  detailParams: CalendarEventEditTemplateDialogParams
+): void => {
+  fireEvent(element, "show-dialog", {
+    dialogTag: "dialog-calendar-template-create",
+    dialogImport: loadCalendarEventEditTemplateDialog,
+    dialogParams: detailParams,
+  });
+};
